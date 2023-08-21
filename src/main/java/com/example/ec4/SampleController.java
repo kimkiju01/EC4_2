e131dbfe3588c140ab775c985a40222a724e7781
kimkiju01 <kimkiju01@gmail.com>
2023-08-21 12:57:50 +0900

    aaa

--- src/main/java/com/example/ec4/SampleController.java
+++ src/main/java/com/example/ec4/SampleController.java
@@ -1,2 +1,16 @@
-package com.example.ec4;public class SampleController {
+package com.example.ec4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class SampleController {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi";
+    }
+
 }
